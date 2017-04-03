aba3e301de4d762f5b20fd50c4462c6237943181
bdrah <badrah-2010@hotmail.com>
2017-04-03 04:30:16 +0300

    change database and login classes

--- app/src/main/java/com/example/bdrah/saviour/DatabaseHelper.java
+++ app/src/main/java/com/example/bdrah/saviour/DatabaseHelper.java
@@ -1,9 +1,11 @@
 package com.example.bdrah.saviour;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.EditText;
 
 /**
  * Created by Bdrahّ on 4/2/2017.
@@ -19,9 +21,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_password = "password";
     private static final String COLUMN_PHONE = "phone";
     private static final String COLUMN_CITY = "city";
+    //private static final String CREATE_QUERY =
+           // "CREATE TABLE "+TABLE_NAME+"("+COLUMN_NAME+" TEXT,"+COLUMN_EMAIL+" TEXT,"+COLUMN_password+" TEXT,"+COLUMN_PHONE+" TEXT,"
+                   // +COLUMN_CITY+" TEXT);";
 
 
-    SQLiteDatabase db;
+    static SQLiteDatabase db;
     private static final String TABLE_CREATE = "create table login(name text not null, email text primary key not null, password text not null, phone text, city text);";
 
 
@@ -46,4 +51,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.onCreate(db);
 
     }
+
+    public  boolean CheckEmailIsDataAlreadyInDBorNot(EditText entEmail) {
+        String Query = "Select email from " + TABLE_NAME + " where " + entEmail+ " = " + "email";
+        Cursor cursor = db.rawQuery(Query, null);
+        if(cursor.getCount() <= 0){
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
+    }
+
+    public  boolean CheckPassIsDataAlreadyInDBorNot(EditText entPass) {
+        String Query = "Select password from " + TABLE_NAME + " where " + entPass+ " = " + "password";
+        Cursor cursor = db.rawQuery(Query, null);
+        if(cursor.getCount() <= 0){
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/example/bdrah/saviour/login.java
+++ app/src/main/java/com/example/bdrah/saviour/login.java
@@ -1,6 +1,9 @@
 package com.example.bdrah.saviour;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class login extends AppCompatActivity implements View.OnClickListener{
+public class login extends AppCompatActivity implements View.OnClickListener  {
    private EditText Email, Password;
    private Button Login;
    private TextView registerLink;
 
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +34,44 @@ public class login extends AppCompatActivity implements View.OnClickListener{
     }
     @Override
     public void onClick(View view) {
-     switch(view.getId()){
-         case R.id.button:
-             startActivity(new Intent(this, Profile.class));
-             break;
+        if(db.CheckEmailIsDataAlreadyInDBorNot(Email))
+            if (db.CheckPassIsDataAlreadyInDBorNot(Password)){
 
-         case R.id.textView:
-             startActivity(new Intent(this, Register.class));
-             break;
+                switch (view.getId()) {
+                    case R.id.button:
+                        startActivity(new Intent(this, Profile.class));
+                        break;
+
+                    case R.id.textView:
+                        startActivity(new Intent(this, Register.class));
+                        break;
 
 
-     }
+                }
+        }
+        else{ if(Email.getText() == null )
+                Email.setError("Please enter your email");
 
+                if(Password.getText() == null)
+                Password.setError("Please enter your password");
+            }
+
+        else{
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+
+            dlgAlert.setMessage("wrong password or username");
+            dlgAlert.setTitle("Error Message...");
+            dlgAlert.setPositiveButton("OK", null);
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+
+            dlgAlert.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+        }
+        }
     }
 
-
-}
